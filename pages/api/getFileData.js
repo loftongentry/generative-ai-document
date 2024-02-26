73b61fc49748c41e5e733804ff52781e263fa345
Lofton <cary.l.gentry.jr@gmail.com>
2024-02-25 21:14:53 -0500

    adjusted JSON

--- pages/api/getFileData.js
+++ pages/api/getFileData.js
@@ -5,6 +5,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ data: workflowResponse })
   } else {
-    return res.status(405).send('Method not allowed')
+    return res.status(405).json({ error: 'Method not allowed' })
   }
 }
