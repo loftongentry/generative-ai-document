4347767a72d09de31a694566c8bfc4cfe5628663
Lofton <cary.l.gentry.jr@gmail.com>
2024-02-25 21:14:46 -0500

    appending uuid to localstorage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DefaultAppBar from "@/components/DefaultAppBar";
 import { Box, Button } from "@mui/material";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session, status } = useSession()
+  const email = session?.user?.email
   const [selectedFile, setSelectedFile] = useState(null)
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      validateUser()
+    }
+  }, [session])
+
+  const validateUser = async () => {
+    try {
+      const res = await fetch(`/api/auth/validate/${email}`, {
+        method: 'GET',
+      })
+
+      if (!res.ok) {
+        throw new Error(`${res.status} - ${res.statusText}`)
+      }
+
+      const response = await res.json()
+      const uuid = response.uuid
+
+      localStorage.setItem('uuid', uuid)
+      
+    } catch (error) {
+      console.error(`Error occurred when logging in user: ${error}`)
+    }
+  }
 
   const handleFileUpload = (event) => {
     setSelectedFile(event.target.files[0])
