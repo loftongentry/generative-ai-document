c556126ced758e41927537b44eeb68c4ed01e0d6
Lofton <cary.l.gentry.jr@gmail.com>
2024-02-25 19:49:10 -0500

    databse connection

--- database/db_def.js
+++ database/db_def.js
@@ -4,12 +4,14 @@ require('dotenv').config({ path: '../.env' })
 const db_address = process.env.DB_ADDRESS
 const db_dialect = process.env.DB_DIALECT
 const db_name = process.env.DB_NAME
+const db_username = process.env.DB_USERNAME
 const db_password = process.env.DB_PASSWORD
-const client_cert = process.env.CLIENT_CERT
-const client_key = process.env.CLIENT_KEY
-const server_ca = process.env.SERVER_CA
+const db_user_password = process.env.DB_USER_PASSWORD
+const client_cert = process.env.CLIENT_CERT.replace(/\\n/g, '\n')
+const client_key = process.env.CLIENT_KEY.replace(/\\n/g, '\n')
+const server_ca = process.env.SERVER_CA.replace(/\\n/g, '\n')
 
-const sequelize = new Sequelize(db_name, db_name, db_password, {
+const sequelize = new Sequelize(db_name, db_username, db_user_password, {
   host: db_address,
   dialect: db_dialect,
   dialectModule: require('pg'),
@@ -38,13 +40,15 @@ const sequelize = new Sequelize(db_name, db_name, db_password, {
 
 module.exports = sequelize
 
-const testConnection = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection established')
-  } catch (error) {
-    console.log('Connection failed', error)
-  }
-}
+// Leaving this here as a reminder on how to test the connection
+// NOTE: Have to cd in the database folder then run 'node db_def.js' to test properly
+// const testConnection = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     console.log('Connection established')
+//   } catch (error) {
+//     console.log('Connection failed', error)
+//   }
+// }
 
-testConnection()
+// testConnection()
