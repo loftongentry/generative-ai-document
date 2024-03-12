515b19a5b539c74922526ce711f5980fac2a36ef
Lofton <cary.l.gentry.jr@gmail.com>
2024-03-11 20:56:49 -0400

    remove now unused imports for theming

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,16 @@
-//TODO: Need to adjust global styling so it's not so bright AND/OR make it so that on button click switches between dark and light mode
-import { SessionProvider } from "next-auth/react"
-import { ThemeProvider, createTheme } from "@mui/material"
-import { SnackbarProvider } from "@/context/SnackbarContext"
-import '../styles/globals.css'
-import '../styles/globals.css'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#0065bd'
-    },
-    secondary: {
-      main: '#00b6d3'
-    }
-  }
-})
+import { SessionProvider } from 'next-auth/react';
+import { SnackbarProvider } from '@/context/SnackbarContext';
+import { ThemeModeProviderComponent } from '@/context/ThemeContext';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+
   return (
     <SnackbarProvider>
       <SessionProvider session={session}>
-        <ThemeProvider theme={theme}>
+        <ThemeModeProviderComponent>
           <Component {...pageProps} />
-        </ThemeProvider>
+        </ThemeModeProviderComponent>
       </SessionProvider>
     </SnackbarProvider>
   )
