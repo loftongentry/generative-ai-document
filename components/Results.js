2d3b458f3e190a819f440dcc5cd71e75491c7730
Lofton <cary.l.gentry.jr@gmail.com>
2024-03-14 21:54:32 -0400

    TODOs for results component

---
+++ components/Results.js
@@ -0,0 +1,10 @@
+//TODO: This is where the results will appears after being returned from the 'getData' api route
+//TODO: If just one page, show results for that one page, if multiple pages, show first page as cumulative results, than can filter down per page, etc.
+
+const Results = () => {
+  return (
+    <div>Results</div>
+  )
+}
+
+export default Results
