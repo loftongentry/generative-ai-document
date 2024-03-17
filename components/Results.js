256b38e371099bc461e47328b9a0ba7a751271b9
Lofton <cary.l.gentry.jr@gmail.com>
2024-03-16 22:09:53 -0400

    forwardRef to use with Slide transition

--- components/Results.js
+++ components/Results.js
@@ -1,10 +1,12 @@
 //TODO: This is where the results will appears after being returned from the 'getData' api route
 //TODO: If just one page, show results for that one page, if multiple pages, show first page as cumulative results, than can filter down per page, etc.
 
-const Results = () => {
+import { forwardRef } from "react"
+
+const Results = forwardRef((props, ref) => {
   return (
     <div>Results</div>
   )
-}
+})
 
 export default Results
