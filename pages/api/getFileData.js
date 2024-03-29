f0034a648e4ec4b438a97d6c253c2b28d526466b
Lofton <cary.l.gentry.jr@gmail.com>
2024-03-29 14:11:59 -0400

    improved function to receive workflow data

--- pages/api/getFileData.js
+++ pages/api/getFileData.js
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const workflowResponse = req.body
-    console.log(`Workflow Response: ${workflowResponse}`)
+  try {
+    const workflowResponse = req.body.data
+    console.log(workflowResponse)
 
-    return res.status(200).json({ data: workflowResponse })
-  } else {
-    return res.status(405).json({ error: 'Method not allowed' })
+    return res.status(200).json({ success: true })
+  } catch (error) {
+    console.error(`Error retrieving data from Google Workflows: ${error}`)
+    return res.status(500).json({ error: 'Internal Server Error' })
   }
 }
