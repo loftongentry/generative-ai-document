4a9448746ff3e28f52e1edc415fa6d5318ffa86b
Lofton <cary.l.gentry.jr@gmail.com>
2024-04-12 14:55:49 -0400

    Improved styling

--- components/Results.js
+++ components/Results.js
@@ -1,6 +1,8 @@
 //TODO: Make sure that the image is not being hidden by the App Bar when not in desktop mode
 //TODO: Clicking on the block with the doc text or the doc summary, opens a modal (or something) so it's easier to read for end user to digest
 //TODO: If document is a PDF, then it's displayed as a PDF displayer. (PNG/JPG/JPEG need to be smaller)
+//TODO: Displaying the analyzed document at the proper ratio of height to width
+//TODO: Making sure if in mobile it's displaying results at proper orientation
 import { forwardRef, useState } from "react"
 import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, Grid, IconButton, Paper, Stack, Typography, Toolbar, styled, Tooltip } from "@mui/material"
 import Image from "next/image"
@@ -9,29 +11,31 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const CustomToolbar = ({ toolbarName, copyToClipboard }) => (
-  <Toolbar
-    disableGutters
-    sx={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      '&.MuiToolbar-root': {
-        minHeight: '0px'
-      }
-    }}
-  >
-    <Typography>
-      {toolbarName}
-    </Typography>
-    <Tooltip
-      title={'Copy Text'}
+  <Item>
+    <Toolbar
+      disableGutters
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        '&.MuiToolbar-root': {
+          minHeight: '0px'
+        }
+      }}
     >
-      <IconButton
-        onClick={copyToClipboard}
+      <Typography>
+        {toolbarName}
+      </Typography>
+      <Tooltip
+        title={'Copy Text'}
       >
-        <ContentCopyIcon />
-      </IconButton>
-    </Tooltip>
-  </Toolbar>
+        <IconButton
+          onClick={copyToClipboard}
+        >
+          <ContentCopyIcon />
+        </IconButton>
+      </Tooltip>
+    </Toolbar>
+  </Item>
 )
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -40,11 +44,14 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   color: theme.palette.text.secondary,
   margin: '5px',
-  padding: '10px'
+  padding: '10px',
+  maxHeight: '500px',
+  maxWidth: '300px',
+  overflow: 'hidden'
 }))
 
 const Results = forwardRef((props, ref) => {
-  const { results, viewportWidth, openSnackbar, resultGridScale } = props
+  const { results, openSnackbar, resultGridScale } = props
   const [langExapanded, setLangExpanded] = useState(false)
   const [wordCountExapnded, setWordCountExpanded] = useState(false)
 
@@ -102,11 +109,27 @@ const Results = forwardRef((props, ref) => {
           item
           xs={resultGridScale ? 12 : 4}
         >
-          <Item>
-            <CustomToolbar
-              toolbarName={'Analyzed Text'}
-              copyToClipboard={() => copyToClipboard(results?.doc_text)}
-            />
+          <CustomToolbar
+            toolbarName={'Analyzed Text'}
+            copyToClipboard={() => copyToClipboard(results?.doc_text)}
+          />
+          <Item
+            sx={{
+              maxHeight: '200px',
+              overflowY: 'scroll',
+              '&::-webkit-scrollbar': {
+                width: '5px',
+              },
+              '&::-webkit-scrollbar-thumb': {
+                borderRadius: '8px',
+                minHeight: '24px',
+                backgroundColor: 'transparent'
+              },
+              '&:hover::-webkit-scrollbar-thumb': {
+                backgroundColor: '#b7b7b7'
+              }
+            }}
+          >
             {results?.doc_text}
           </Item>
         </Grid>
@@ -114,11 +137,27 @@ const Results = forwardRef((props, ref) => {
           item
           xs={resultGridScale ? 12 : 4}
         >
-          <Item>
-            <CustomToolbar
-              toolbarName={'Document Summary'}
-              copyToClipboard={() => copyToClipboard(results?.doc_summary)}
-            />
+          <CustomToolbar
+            toolbarName={'Document Summary'}
+            copyToClipboard={() => copyToClipboard(results?.doc_summary)}
+          />
+          <Item
+            sx={{
+              maxHeight: '200px',
+              overflowY: 'scroll',
+              '&::-webkit-scrollbar': {
+                width: '5px',
+              },
+              '&::-webkit-scrollbar-thumb': {
+                borderRadius: '8px',
+                minHeight: '24px',
+                backgroundColor: 'transparent'
+              },
+              '&:hover::-webkit-scrollbar-thumb': {
+                backgroundColor: '#b7b7b7'
+              }
+            }}
+          >
             {results?.doc_summary}
           </Item>
         </Grid>
