99c1361a5a24c7483367c0eaff443a641b6a5a14
Lofton <cary.l.gentry.jr@gmail.com>
2024-04-16 21:39:45 -0400

    Batch delete document API route

--- pages/api/firestore/delete-all/[uuid].js
+++ pages/api/firestore/delete-all/[uuid].js
@@ -1,1 +1,30 @@
-//TODO: API route to delete all documents with user's id
+//TODO: Need to test
+import { firestore } from "googleapis/build/src/apis/firestore"
+import { getToken } from "next-auth/jwt"
+
+const collection = process.env.NODE_ENV === 'development' ? 'Testing Storage' : ''
+
+export default async function handler(req, res) {
+  const { query: { uuid } } = req
+  const token = await getToken({ req })
+
+  if (!token) {
+    return res.status(401).json({ error: 'User access denier' })
+  }
+
+  try {
+    const querySnapshot = await firestore.collection(`${collection}`).where('user_uuid', '==', uuid).get()
+
+    const batch = firestore.batch()
+
+    querySnapshot.forEach(doc => {
+      batch.delete(doc.ref)
+    })
+
+    await batch.commit()
+    return res.status(200).json({ success: true })
+  } catch (error) {
+    console.error(`Error deleting all analysis for user with ID: ${uuid}: ${error}`)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
