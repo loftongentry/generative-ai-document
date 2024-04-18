312bce85c82665d2366cbe0b9335ba8adcf31c9c
Lofton <cary.l.gentry.jr@gmail.com>
2024-04-17 20:53:18 -0400

    1. Delete all entries in firestore with that uuid 2. Delete user from user database with that uuid

--- pages/api/auth/delete-account/[uuid].js
+++ pages/api/auth/delete-account/[uuid].js
@@ -1,1 +1,38 @@
-//TODO: Need to delete all analysis and delete account from database
+//TODO: Need to test
+import { User } from "@/database/models";
+import { firestore } from "googleapis/build/src/apis/firestore"
+import { getToken } from "next-auth/jwt"
+
+const collection = process.env.NODE_ENV === 'development' ? 'Testing Storage' : ''
+
+export default async function handler(req, res) {
+  const { query: { uuid } } = req
+  const token = await getToken({ req })
+
+  if (!token) {
+    return res.status(401).json({ error: 'User access denier' })
+  }
+
+  try {
+    const querySnapshot = await firestore.collection(`${collection}`).where('user_uuid', '==', uuid).get()
+
+    const batch = firestore.batch()
+
+    querySnapshot.forEach(doc => {
+      batch.delete(doc.ref)
+    })
+
+    await batch.commit()
+
+    await User.destroy({
+      where: {
+        uuid: uuid
+      }
+    })
+
+    return res.status(200).json({ success: true })
+  } catch (error) {
+    console.error(`Error deleting all analysis for user with ID: ${uuid}: ${error}`)
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+}
