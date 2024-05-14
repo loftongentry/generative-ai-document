757ac555c50a5c946519caca0c4a745102b741be
Lofton <cary.l.gentry.jr@gmail.com>
2024-05-13 20:28:14 -0400

    config for next/image for stuff from gcp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+//TODO: Need to add to the proper bucket for production in here
 /** @type {import('next').NextConfig} */
+const pathname = process.env.NODE_ENV === 'development' ? '/gen-ai-doc-bucket-1-testing/**' : ''
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: pathname
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
