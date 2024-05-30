af167cda55022a2bb9bf673c00537789a932732b
Lofton <cary.l.gentry.jr@gmail.com>
2024-05-30 12:20:09 -0400

    properly catching errors

--- pages/api/get-file-data.js
+++ pages/api/get-file-data.js
@@ -1,3 +1,4 @@
+//NOTE: Pretty sure this will properly catch errors when they are received from google workflow, but we ball regardless
 let workflowData = null
 
 export default async function handler(req, res) {
@@ -7,10 +8,17 @@ export default async function handler(req, res) {
     try {
       workflowData = req
 
+      const body = workflowData.body
+      const data = body.data
+
+      if (data && typeof data === 'string' && data.includes('error')) {
+        throw new Error(data)
+      }
+
       return res.status(200).json({ success: true })
     } catch (error) {
       console.error(`Error trying to receive data from workflow: ${error}`)
-      return res.status(400).json({ error: error })
+      return res.status(400).json({ success: false })
     }
   }
 
@@ -18,17 +26,23 @@ export default async function handler(req, res) {
     try {
       if (workflowData === null) {
         console.log('polling')
+
         return res.status(200).json({ status: 'pending' })
       } else {
         console.log('retrieved data')
 
         const data = workflowData.body.data
         workflowData = null
+
+        if (data.includes('error')) {
+          throw new Error(data)
+        }
+
         return res.status(200).json({ status: 'ready', data: data })
       }
     } catch (error) {
       console.error(`Error trying to send data from workflow: ${error}`)
-      return res.status(500).json({ error: error })
+      return res.status(500).json({ data: `${error}` })
     }
   }
 
